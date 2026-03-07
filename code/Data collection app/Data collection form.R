8bf64e608d74c2a5918e11e0cd2fb8d88cb1591b
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-03-07 12:57:55 +0300

    Initial commit, creating data collection app with Rshiny.

---
+++ code/Data collection app/Data collection form.R
@@ -0,0 +1,17 @@
+setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\shiny\\code\\Data collection app")
+
+#load libraries
+library(shiny)
+
+#define ui
+ui <-
+  
+  
+
+  
+#define server logic
+server <-
+  
+
+  
+#run the application
