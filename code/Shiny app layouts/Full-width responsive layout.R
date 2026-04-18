6a89acc4f08c43028dcf425bf200535547c35cb4
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-04-18 18:05:24 +0300

    creating a full-width response Rshiny layout.

--- code/Shiny app layouts/Full-width responsive layout.R
+++ code/Shiny app layouts/Full-width responsive layout.R
@@ -5,30 +5,30 @@ library(shiny)
 library(bslib)
 
 #define ui
-ui <- page_sidebar(
-  sidebar = sidebar(
-    title = "Filters"
-  ),
-  
-  #main content
+ui <- page_fillable(
+  #1st row: 4 cards
   layout_columns(
     col_widths = c(3,3,3,3),
     card(),card(),card(),card()
   ),
+  
+  #2nd row: 3 cards
   layout_columns(
     col_widths = c(4,4,4),
     card(),card(),card()
   ),
+  
+  #3rd row: 2 cards
   layout_columns(
     col_widths = c(6,6),
     card(),card()
   )
 )
-  
+
 #define server
 server <- function(input, output, session){
   
 }
-  
+
 #run the application
 shinyApp(ui = ui, server = server)
