ddf1dbea39d597257edee39c0501c5df03d73b4c
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-04-26 19:44:30 +0300

    Adding navigation bar to Rshiny dashboard.

--- code/Shiny app layouts/Full-width responsive layout_2cards.R
+++ code/Shiny app layouts/Full-width responsive layout_2cards.R
@@ -4,12 +4,20 @@ setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\shiny\\code\\Shiny app l
 library(shiny)
 library(bslib)
 
-ui <- page_fillable(
+ui <- page_navbar(
+  title = "Executive Sales Dashboard",
+  theme = bs_theme(version = 5, bootswatch = "flatly"),
+  
+  nav_panel(
+    NULL, #hides tab label
+  page_fillable(
   #1st row: 2 cards
   layout_columns(
     col_widths = c(3,9),
     card(),card()
   )
+)
+)
 )
 
 #define server
