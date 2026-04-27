b733cda459a841d0406a3f51ce43e0224095e414
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-04-27 16:13:26 +0300

    Adding text details and filter input to improve the app.

--- code/Shiny app layouts/Full-width responsive layout_2cards.R
+++ code/Shiny app layouts/Full-width responsive layout_2cards.R
@@ -14,7 +14,25 @@ ui <- page_navbar(
   #1st row: 2 cards
   layout_columns(
     col_widths = c(3,9),
-    card(),card()
+    card(
+      # ---- Paragraph 1 ----
+      p("At Rigor Data Solutions, we support you unleash the true power of your data and automate internal processes through modern business intelligence and analytics solutions. Our projects are designed, developed, and deployed by experienced professionals who are tested and certified in their respective fields.
+        With almost a decade of unparalleled experience in building data analytics and visualization solutions, we bring expertise, professionalism, and passion for turning complexity into clarity."),
+      
+      # ---- Filter Input ----
+      selectInput(
+        inputId = "region",
+        label = "Select Region",
+        choices = c("Nairobi", "Mombasa", "Kisumu","Nakuru","Eldoret"),
+        width = "100%"
+      ),
+      
+      # ----- Paragraph 2 -----
+      p(
+        "At Rigor Data Solutions, we prioritize quality above all else. Our commitment is to deliver exceptional results that exceed your expectations. View some of our sample visualization below."
+      )
+    ),
+    card()
   )
 )
 )
