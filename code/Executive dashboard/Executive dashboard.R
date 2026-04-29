9e8c03a3fb58a0e73d489735b3ca64b5e7f8a608
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-04-29 16:00:04 +0300

    initial commit - creating an executive dashboard

---
+++ code/Executive dashboard/Executive dashboard.R
@@ -0,0 +1,42 @@
+setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\shiny\\code\\Executive dashboard")
+
+#load libraries
+library(shiny)
+library(bslib)
+
+#define ui
+ui <- page_navbar(
+  title = "Executive Sales Dashboard",
+  theme = bs_theme(version = 5, bootswatch = "flatly"),
+  
+  nav_panel(
+    NULL, #hides tab label
+    page_fillable(
+      #1st row: 4 cards
+      layout_columns(
+        col_widths = c(4,4,4),
+        card(),card(),card()
+      ),
+      
+      #2nd row: 3 cards
+      layout_columns(
+        col_widths = c(4,4,4),
+        card(),card(),card()
+      ),
+      
+      #3rd row: 2 cards
+      layout_columns(
+        col_widths = c(4,4,4),
+        card(),card(),card()
+      )
+    )
+  )
+)
+
+#define server
+server <- function(input, output, session){
+  
+}
+
+#run the application
+shinyApp(ui = ui, server = server)
