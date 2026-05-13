c2850da676876158aca7988bc160711d9c766c92
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-05-13 16:45:55 +0300

    Initial commit on survey dashboard.

---
+++ code/Survey dashboard/Survey Dashboard.R
@@ -0,0 +1,22 @@
+setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\shiny\\code\\Survey dashboard")
+#load libraries
+library(shiny)
+library(bslib)
+
+#define ui
+ui <- fluidPage(
+  
+)
+
+
+#define server
+server <- function(input, output, session){
+  
+}
+
+
+#run application
+shinyApp(ui = ui, server = server)
+
+
+
