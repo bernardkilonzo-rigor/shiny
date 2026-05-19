001b89973aa5953ff578d469d721c4b53cc37da6
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-05-19 16:43:22 +0300

    Creating a shiny app layout with two tabs namely; Demographic dashboard and Survey dashboard.

--- code/Survey dashboard/Survey Dashboard.R
+++ code/Survey dashboard/Survey Dashboard.R
@@ -9,10 +9,121 @@ survey_data <-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/d
   clean_names()
 
 #define ui
-ui <- fluidPage(
+ui <- page_navbar(
+  title = "Survey Analysis Dashboard",
+  theme = bs_theme(version = 5, bootswatch = "flatly"),
   
+  # Demographic Tab
+  nav_panel(
+    title = "Demographic Dashboard",
+    page_fillable(
+      # 1st row: 3 cards for demographic overview
+      layout_columns(
+        col_widths = c(4, 4, 4),
+        card(
+          card_header(),
+          card_body()
+        ),
+        card(
+          card_header(),
+          card_body()
+        ),
+        card(
+          card_header(),
+          card_body()
+        )
+      ),
+      
+      # 2nd row: 2 larger cards
+      layout_columns(
+        col_widths = c(6, 6),
+        card(
+          card_header(),
+          card_body()
+        ),
+        card(
+          card_header(),
+          card_body()
+        )
+      ),
+      
+      # 3rd row: 1 full-width card
+      layout_columns(
+        col_widths = 12,
+        card(
+          card_header(),
+          card_body()
+        )
+      )
+    )
+  ),
+  
+  # Survey Dashboard Tab
+  nav_panel(
+    title = "Survey Dashboard",
+    page_sidebar(
+      sidebar = sidebar(
+        title = "Filters",
+        open = "closed", #sidebar is hidden by default
+        
+        #Gender filter
+        
+      ),
+      
+      # Main content area
+      # 1st row: 4 metric cards
+      layout_columns(
+        col_widths = c(3, 3, 3, 3),
+        card(
+          card_header(),
+          card_body()
+        ),
+        card(
+          card_header(),
+          card_body()
+        ),
+        card(
+          card_header(),
+          card_body()
+        ),
+        card(
+          card_header(),
+          card_body()
+        )
+      ),
+      
+      # 2nd row: 3 visualization cards
+      layout_columns(
+        col_widths = c(4, 4, 4),
+        card(
+          card_header(),
+          card_body()
+        ),
+        card(
+          card_header(),
+          card_body()
+        ),
+        card(
+          card_header(),
+          card_body()
+        )
+      ),
+      
+      # 3rd row: 2 detailed analysis cards
+      layout_columns(
+        col_widths = c(6, 6),
+        card(
+          card_header(),
+          card_body()
+        ),
+        card(
+          card_header(),
+          card_body()
+        )
+      )
+    )
+  )
 )
-
 
 #define server
 server <- function(input, output, session){
