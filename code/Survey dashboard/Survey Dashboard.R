52679ab1b3bc87f9c12c37ac1096c94c6e39311b
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-06-18 17:07:03 +0300

    Adding percent labels on the viz on % of course completion.

--- code/Survey dashboard/Survey Dashboard.R
+++ code/Survey dashboard/Survey Dashboard.R
@@ -504,6 +504,8 @@ server <- function(input, output, session){
         ggplot(aes(y = Quiz, x = Percent, fill = Responses))+
         geom_bar(stat = "identity", position = "stack")+
         scale_x_continuous(labels = percent_format())+
+        geom_text(aes(label = Percent), position = position_stack(vjust = 0.5),
+                  color = "white", size =3.5)+
         scale_fill_paletteer_d("nationalparkcolors::Acadia")+
         theme(
           panel.background = element_blank(),
